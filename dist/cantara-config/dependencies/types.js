8031fdcbaab60ed0a1c216b9b2bc3a2c9f23d728
Maximilian Torggler <maximilian.torggler@tuta.io>
2020-01-23 17:13:58 +0700

    Fixed @types version missmatch.

--- dist/cantara-config/dependencies/types.js
+++ dist/cantara-config/dependencies/types.js
@@ -3,8 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 /** All typescript dependencies */
 exports.typescriptDependencies = {
     '@types/node': '^12.0.0',
-    '@types/react': '^16.9.0',
-    '@types/react-dom': '^16.9.0',
+    '@types/react': '*',
+    '@types/react-dom': '*',
     '@types/jest': '^24.9.0',
     typescript: '^3.7.2',
 };
--- src/cantara-config/dependencies/types.ts
+++ src/cantara-config/dependencies/types.ts
@@ -1,8 +1,8 @@
 /** All typescript dependencies */
 export const typescriptDependencies = {
   '@types/node': '^12.0.0',
-  '@types/react': '^16.9.0',
-  '@types/react-dom': '^16.9.0',
+  '@types/react': '*',
+  '@types/react-dom': '*',
   '@types/jest': '^24.9.0',
   typescript: '^3.7.2',
 };
