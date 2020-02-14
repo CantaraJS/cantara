805b2f7086aff9ef99760368c58dc544919bbe3e
Maximilian Torggler <maximilian.torggler@tuta.io>
2020-02-14 18:12:13 +0700

    Deploy website version based on 7eb9d0161fb238069783367e8be7734f7dbff5ba

---
+++ 11ab0ec2.6b9b1494.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{98:function(a){a.exports=JSON.parse('{"allTagsPath":"/cantara/blog/tags","slug":"facebook","name":"facebook","count":1,"permalink":"/cantara/blog/tags/facebook"}')}}]);
---
+++ fc9b0af6.0710adbb.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[43],{135:function(a){a.exports=JSON.parse('{"permalink":"/cantara/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":3,"previousPage":null,"nextPage":null}')}}]);
