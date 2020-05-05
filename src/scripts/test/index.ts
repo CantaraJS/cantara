412502e983ea06fe3f0e4dc396aee9a29affe3ba
Timo Angerer <code@timoangerer.io>
2020-05-05 12:13:07 +0200

    feat: add VSCode debug support for Jest tests

--- src/scripts/test/index.ts
+++ src/scripts/test/index.ts
@@ -1,3 +1,4 @@
+import path from 'path';
 import execCmd from '../../util/exec';
 import getRuntimeConfig from '../../cantara-config/runtime-config';
 import getGlobalConfig from '../../cantara-config/global-config';
@@ -7,9 +8,24 @@ export default function executeTests() {
     currentCommand: { app: activeApp },
   } = getRuntimeConfig();
 
-  const { additionalCliOptions } = getGlobalConfig();
+  const {
+    additionalCliOptions,
+    internalPaths: { root: cantaraRootPath },
+  } = getGlobalConfig();
+  const jestInstallPath = path.join(
+    cantaraRootPath,
+    'node_modules/jest/bin/jest.js',
+  );
 
-  const cmdToExecute = `jest ${additionalCliOptions}`;
+  let newAdditionalCliOptions = additionalCliOptions;
+  let nodeCmd = '';
+
+  if (newAdditionalCliOptions.includes('--ctra-debug')) {
+    nodeCmd = `node --inspect-brk=9239 `;
+    newAdditionalCliOptions = newAdditionalCliOptions.replace('--ctra-debug', '');
+  }
+    
+  const cmdToExecute = `${nodeCmd}"${jestInstallPath}" ${newAdditionalCliOptions}`;
 
   execCmd(cmdToExecute, {
     workingDirectory: activeApp.paths.root,
