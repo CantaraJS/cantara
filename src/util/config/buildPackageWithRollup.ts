75aa2873a3837251a03c86c279f31b69df3fc18a
Maximilian Torggler <maximilian.torggler@tuta.io>
2021-08-23 12:33:56 +0200

    fix: use custom dynamic import plugin for rollup

--- src/util/config/buildPackageWithRollup.ts
+++ src/util/config/buildPackageWithRollup.ts
@@ -8,8 +8,7 @@ import alias from '@rollup/plugin-alias';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import { babel } from '@rollup/plugin-babel';
-//@ts-expect-error
-import dynamicImportVars from '@rollup/plugin-dynamic-import-vars';
+import dynamicImportVars from 'rollup-plugin-dynamic-import-vars-for-grown-ups';
 //@ts-expect-error
 import url from '@rollup/plugin-url';
 import postcss from 'rollup-plugin-postcss-modules';
